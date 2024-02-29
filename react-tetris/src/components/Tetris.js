41ce49c5904397d8288f06bfe369a309d25bc4e6
Ethan Phang <epha0016@student.monash.edu>
2024-03-01 00:38:30 +1100

    Created hooks and implemented inside Tetris main game

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { createStage } from '../gameHelpers';
+//Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
 // Components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton'
+
+// Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 /**
  * Represents the Tetris game component.
  * @returns {JSX.Element} The Tetris game component.
  */
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const[stage, setStage] = useStage(player);
+
+
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage = {createStage()} />
+                <Stage stage = {stage} />
                 <aside>
-                    <div>
-                        <Display text="Score" />
-                        <Display text="Rows" />
-                        <Display text="Level" />
-                    </div>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text= "Gam Over" />
+                        ) : (
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    )}
                     <StartButton />
                 </aside>
             </StyledTetris>
