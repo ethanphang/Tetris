cfe47dcf37ebdec28f04f9020295b23d86a2a11c
Ethan Phang <epha0016@student.monash.edu>
2024-03-01 17:40:01 +1100

    Added collision detection

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 //Custom Hooks
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
@@ -26,11 +26,13 @@ const Tetris = () => {
 
     // Custom hooks for managing player and stage state
     const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [stage, setStage] = useStage(player, resetPlayer);
 
     // Function to move the player left or right
     const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0 });
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 });
+        }
     };
 
     // Function to start the game
@@ -38,11 +40,20 @@ const Tetris = () => {
         // Reset the stage and player
         setStage(createStage());
         resetPlayer();
+        setGameOver(false);
     };
 
     // Function to drop the player tetromino down
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false });
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false });
+        } else {
+            if (player.pos.y < 1) {
+                setGameOver(true);
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
     };
 
     // Function to handle dropping the player tetromino
--- react-tetris/src/gameHelpers.js
+++ react-tetris/src/gameHelpers.js
@@ -5,7 +5,25 @@ export const STAGE_HEIGHT = 20;
  * Creates a new game stage.
  * @returns {Array<Array<[number, string]>>} An array representing the game stage.
  */
-export const createStage = () => 
+export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
     )
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+    for (let y = 0; y < player.tetromino.length; y += 1) {
+        for (let x = 0; x < player.tetromino[y].length; x += 1) {
+            //Check that we're on tetrimino cell
+            if (player.tetromino[y][x] !== 0) {
+                //Check that our move is valid
+                if (
+                    !stage[y + player.pos.y + moveY] ||
+                    !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                    stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+}
